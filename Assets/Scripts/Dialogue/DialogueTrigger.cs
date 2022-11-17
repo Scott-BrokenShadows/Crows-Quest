be4fe8aeb7eecaa2307cc1f66cafba538650649d
indiejobbs <indiejobbs@gmail.com>
2022-11-17 11:13:15 +0800

    Fixed some UI

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -8,9 +8,12 @@ public class DialogueTrigger : MonoBehaviour
     
 
     public GameObject visualCue;
-    
-
     public bool playerInRange;
+    // Set Bounce Boundaries
+    float noticeMe = 0.0001f;
+    float noticeVal = 0f;
+    float noticeMax = 0.005f;
+    float noticeMin = -0.005f;
 
     private void Awake()
     {
@@ -29,6 +32,11 @@ public class DialogueTrigger : MonoBehaviour
         {
             visualCue.SetActive(false);
         }
+        // Bounce UI Icon
+        noticeVal += noticeMe;
+        if (noticeVal >= noticeMax) noticeMe = -noticeMe; 
+        if (noticeVal <= noticeMin) noticeMe = -noticeMe;
+        visualCue.transform.position += new Vector3 (0, noticeVal, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,11 +12,13 @@ public class PlayerController : MonoBehaviour
 
     public float inputX;
     public float inputY;
+    public SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get Renderer for Sprite Flip
+        sprite = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,6 +30,9 @@ public class PlayerController : MonoBehaviour
         }
 
         theRB.velocity = new Vector2(inputX * moveSpeed, inputY * moveSpeed);
+        // Flip Sprite with X Input
+        if (inputX < 0) sprite.flipX = true; 
+        if (inputX > 0) sprite.flipX = false;
     }
 
     public void Move(InputAction.CallbackContext context)
