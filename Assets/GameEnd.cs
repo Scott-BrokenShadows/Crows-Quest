302641fdc7c47720c1315a41a95e22525167e81c
indiejobbs SAE <indiejobbs@gmail.com>
2022-12-15 13:53:16 +0800

    Rebuilt with end scene

--- Assets/GameEnd.cs
+++ Assets/GameEnd.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameEnd : MonoBehaviour
 {
     public GameObject endPanel;
+    public TextMeshProUGUI coinAmount;
+    public TextMeshProUGUI keyAmount;
+    public TextMeshProUGUI endText;
 
     private void Start()
     {
@@ -14,7 +19,16 @@ public class GameEnd : MonoBehaviour
     {
         if (collision.gameObject.name == "Potion")
         {
-            endPanel.SetActive(true);
+            StartCoroutine(EndGame());
         }
     }
+
+    IEnumerator EndGame() {
+        endPanel.SetActive(true);
+        endText.text = "You did it! You found " + coinAmount.text + "/20 coins and " + keyAmount.text + "/5 keys!";
+
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(0);
+    }
+
 }
